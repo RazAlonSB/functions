4e43d445a34308a2baabaf28b52ae01a3f1692ed
raz <raz@sparkbeyond.com>
2016-03-28 17:40:42 +0300

    object

--- Functions.scala
+++ Functions.scala
@@ -1,4 +1,5 @@
-  def portfolioTsTs[K1, K2](portfolio: Iterable[(K1, Double)], ts1: TimeSeries[K2], ts2: TimeSeries[Double]) = {
+object MyObject {  
+def portfolioTsTs[K1, K2](portfolio: Iterable[(K1, Double)], ts1: TimeSeries[K2], ts2: TimeSeries[Double]) = {
 
     val tsPortfolio = core.Solving.SpecialFunctions.timeSeriesValuesDistributionMap(ts1, ts2)
 
@@ -16,3 +17,4 @@
 
     core.Solving.SpecialFunctions.crossProduct(rankedPortfolio.take(3).map(_._1), rankedTSPortfolio.take(3).map(_._1)).map { x => (x._1, x._2) -> portfolio.toMap.getOrElse(x._1, 0.0) / tsPortfolio.toMap.getOrElse(x._2, 1.1) }.toMap
   }
+}
