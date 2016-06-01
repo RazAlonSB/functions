c7e9a9d3a86c5535789090366c26eab814155606
raz <raz@sparkbeyond.com>
2016-06-01 15:00:19 +0300

    return value

--- Functions2.scala
+++ Functions2.scala
@@ -1,4 +1,4 @@
-def mapmap(m1: Map[com.sparkbeyond.runtime.feature.types.Text, Double], m2:   Map[com.sparkbeyond.runtime.feature.types.Text,Double]) = {
+def mapmap(m1: Map[com.sparkbeyond.runtime.feature.types.Text, Double], m2:   Map[com.sparkbeyond.runtime.feature.types.Text,Double]): Seq[com.sparkbeyond.runtime.feature.types.Text] = {
   m1.keys.toSet.intersect(m2.keys.toSet).toSeq
 }
 
