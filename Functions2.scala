41b97226daa164e4e5c5134901f49285afcbbf3f
raz <raz@sparkbeyond.com>
2016-06-01 13:05:22 +0300

    full module path

--- Functions2.scala
+++ Functions2.scala
@@ -1,4 +1,4 @@
-def mapmap(m1: Map[Text, Double], m2:   Map[Text,Double]) = {
+def mapmap(m1: Map[com.sparkbeyond.runtime.feature.types.Text, Double], m2:   Map[com.sparkbeyond.runtime.feature.types.Text,Double]) = {
   m1.keys.toSet.intersect(m2.keys.toSet).toSeq
 }
 
