b92f28ec8332f6125856cf739f55b67cddafe87b
RazAlonSB <raz@sparkbeyond.com>
2016-09-08 13:49:44 +0300

    Delete AeonExtensions.scala

--- AeonExtensions.scala
+++
@@ -1,6 +0,0 @@
-object Functions {
-	@ZeroComplexity
-	def valuesOfAttributesOfObjectsInCircle(c: GeoCircle): scala.collection.Map[String, Double] = attributesOfNearestObjects(c).map{
-		case ((l,r), d: Double) => r.toString->d
-	}
-}
