a739ee13d6abc7cf74f06b03ebbc1a1412146f34
raz <raz@sparkbeyond.com>
2016-09-11 13:12:37 +0300

    changed object name

--- OSMExtensions.scala
+++ OSMExtensions.scala
@@ -2,20 +2,18 @@ import com.sparkbeyond.runtime.feature.types._
 import com.sparkbeyond.runtime.util.datastructures.geo._
 import com.sparkbeyond.runtime.graph._
 
-object MyZeroComplexityFunctions {
+object MyZeroComplexityFunctions2 {
 
 	def valuesOfAttributesOfObjectsInCircle(c: GeoCircle, osm: OpenStreetMapTrait): scala.collection.Map[String, Double] = {
 
-		osm.attributesOfNearestObjects(c).map{
-		case ((l,r), d: Double) => r.toString->d
+		osm.attributesOfNearestObjects(c).map{ case ((l,r), d: Double) => r.toString->d }
 	}
-}
 
 	def valuesOfAttributesOfObjectsDistributionInCircle(c: GeoCircle, osm: OpenStreetMapTrait): Histogram[(String, Any)] = new Histogram(valuesOfAttributesOfObjectsInCircle(c,osm))
-/*
+
 	def mapKeyToLookupValue(map: scala.collection.Map[String, Double], lookupTable: MapWrapper[String, Text]): scala.collection.GenSeq[(String	, Double)] = {
 		println("<<<<<<< Hey, I'm here >>>>>>>>>>>")
 		map.toSeq.map { case (key, d) => lookupTable.get(key).getOrElse("").toString -> d }
 	}
-*/
+
 }
